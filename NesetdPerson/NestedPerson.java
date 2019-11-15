aa8c84e7e9a79702b5b5699b951a242b7cbd6e44
mazmich <36936745+mazmich@users.noreply.github.com>
2019-11-15 12:06:25 +0100

    Zadanie 6

--- NesetdPerson/NestedPerson.java
+++ NesetdPerson/NestedPerson.java
@@ -1,10 +1,10 @@
 
-//Stwórz klasę Person i kilka pól i metod do niej.
-//Dodatkowo stwórz wewnętrznš klasę HealthSonda, którš to będzie można pobrać za pomocš jednej z metod
-//klasy Person - a będzie udostępniała tylko wybrane informacje na temat klasy Person
-//(których lekarz może być zainteresowany) - bez potrzeby podawania danych, które on nie powinien widzieć ze względu
-//np. na ustawę o ochronie danych osobowych - a jak byłoby to w przypadki zwrócenia po prostu klasy Person.
-//Zaprezentuj w NesetedPerson tš implementację.
+// Stwórz klasę Person i kilka pól i metod do niej.
+// Dodatkowo stwórz wewnętrzną klasę HealthSonda, którą to będzie można pobrać za pomocą jednej z metod
+// klasy Person - a będzie udostępniała tylko wybrane informacje na temat klasy Person
+// (których lekarz może być zainteresowany) - bez potrzeby podawania danych, które on nie powinien widzieć ze względu
+// np. na ustawę o ochronie danych osobowych - a jak byłoby to w przypadki zwrócenia po prostu klasy Person.
+// Zaprezentuj w NesetedPerson tą implementację.
 
 public class NestedPerson {
 	
