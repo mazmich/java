61ad41a781fc75247507d0f79f604fb129c60558
mazmich <36936745+mazmich@users.noreply.github.com>
2019-12-07 09:31:00 +0100

    Zadanie 3

---
+++ Zajęcia 7.12/Maps.java
@@ -0,0 +1,14 @@
+// Przeczytaj artyku³ https://kobietydokodu.pl/niezbednik-juniora-kolekcje-w-jezyku-java/,
+// a nstêpnie wykonaj zadanie.
+
+// Stwórz mapê typu: Map<Integer, Person> gdzie Key=numer PESEL, Value=Person.
+// Poka¿ jak ³atwo z wykorzystaniem Mapy mo¿na znajdowaæ osoby po numerach PESEL.
+// Zaprezentuj w main korzystnie z tego mechanizmu.
+
+public class Maps {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
