45c945fa9c74adb80f6b746ccd7604ae5c5ad1c4
mazmich <36936745+mazmich@users.noreply.github.com>
2019-12-07 09:26:44 +0100

    Zadanie 3

---
+++ Zajęcia 7.12/People.java
@@ -0,0 +1,14 @@
+// Stwórz klasê People (imiê, nazwisko, wiek).
+// Utwórz List<People> i wrzuæ tam kilka elementów.
+//	1) Spraw aby drukuj¹c wszystkie elementy: linijka System.out.println(person); drukowa³a dane o osobie w przystepnej formie.
+//	2) Poka¿ przyk³ad sortowania malej¹co wg. wieku za pomoc¹ interfejsu Comparable
+//	3) Poka¿ przyk³ad sortowania malej¹co wg. nazwiska osoby przy u¿yciu komparatora
+
+
+public class People {
+	
+	public static void main(String[] args) {
+	    System.out.println("Hello world!");
+	  }
+
+}
