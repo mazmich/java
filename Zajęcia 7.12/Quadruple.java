6cfd651182e1a25ee80ac85f5ad8c38b5d18fa70
mazmich <36936745+mazmich@users.noreply.github.com>
2019-12-07 09:26:00 +0100

    Zadanie 2

---
+++ Zajęcia 7.12/Quadruple.java
@@ -0,0 +1,9 @@
+public class Quadruple {
+	
+  public static void main(String[] args) {
+	  
+	  // Korzystaj¹c z klas: Pair oraz Triple (mo¿esz je tu wkleiæ) - stwórz klasê Quadruple,
+	  //która jest uniwersalnym kontenerem dla 4 typów generycznych. 
+	  // W main poka¿ u¿ycie. 
+  }
+}
