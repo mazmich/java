9d2e5bf916cc96724d8e3d7e847bdafb68460ca1
mazmich <36936745+mazmich@users.noreply.github.com>
2019-12-07 09:29:13 +0100

    Zadanie 4

--- Zajęcia 7.12/People.java
+++ Zajęcia 7.12/People.java
@@ -1,14 +1,16 @@
-// Stwórz klasê People (imiê, nazwisko, wiek).
-// Utwórz List<People> i wrzuæ tam kilka elementów.
-//	1) Spraw aby drukuj¹c wszystkie elementy: linijka System.out.println(person); drukowa³a dane o osobie w przystepnej formie.
-//	2) Poka¿ przyk³ad sortowania malej¹co wg. wieku za pomoc¹ interfejsu Comparable
-//	3) Poka¿ przyk³ad sortowania malej¹co wg. nazwiska osoby przy u¿yciu komparatora
+//Dla klasy People zobacz jak dzia³a HashSet.
+//	1) Dodaj kilka osób do zbioru
+//	2) Stwórz dwa obiekty  tak samo siê nazywaj¹ce i maj¹ce tyle samo lat (takie same osoby).
+//		Spróbuj dodaæ te elementy do zbioru. Uda³o siê?
+//	3) Zaimplementuj poprawnie hashCode() oraz equals(...)
+//		i sprawdŸ teraz czy siê powy¿sza procedura powiod³a
+//	4) Utwórz TreeSet dla People i niech bêdzie domyœlnie
+//		sortowany po Comparable (wiek rosn¹co)
+//	5) Utwórz kolejny TreeSet ale posortowany po wieku malej¹co.
 
 
 public class People {
-	
-	public static void main(String[] args) {
-	    System.out.println("Hello world!");
-	  }
 
+	// miejsce na Twój kod
+	
 }
