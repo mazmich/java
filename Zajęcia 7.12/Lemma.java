e5799dfac4b20ac10ea5de20a5eb7df07f91dccb
mazmich <36936745+mazmich@users.noreply.github.com>
2019-12-07 09:36:11 +0100

    Praca domowa

---
+++ Zajęcia 7.12/Lemma.java
@@ -0,0 +1,20 @@
+// Zróbmy prosty lemmatyzator tekstu. Co to jest? To jest mechanizm sprowadzania tekstu do form podstawowych. Przyk³ad:
+
+// 		Ala posz³a do lasu i nazbiera³a grzybów.
+
+// zostanie zlemmatyzowane do:
+
+// 		alicja pójœæ do las zbieraæ grzyb
+
+// Zainicjuj mapê odpowiednimi danymi K=S³owo, V=Lemma (wymyœl je sam).
+// Zaprezentuj pracê lemmatyzatora w akcji.
+// U¿yj StringTokenizera aby oddzieliæ s³owa w zdaniu.
+// U¿yj LowerCase dla Stringów aby nie byæ wra¿liwym na ma³e i du¿e listery w wyrazach.
+// SprawdŸ co siê stanie gdy dodamy nowy element pod ten sam klucz podczas budowania Mapy (s³ownika).
+// Zadbaj aby klasy mia³y poprawnie zaimplementowne hashCode i equals.
+// Zaprezentuj dzia³anie takiego zestawu.
+
+
+public class Lemma {
+
+}
